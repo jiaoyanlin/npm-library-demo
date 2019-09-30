f4fac500a73d04a6f872442a6763e3983e471d83
jiaoyanlin <2582016686@qq.com>
2019-09-30 22:21:54 +0800

    feat: 完善自动生成changelog并打标签命令

--- build/version.js
+++ build/version.js
@@ -1,18 +1,12 @@
-const fs = require('fs');
 const path = require('path');
 const pathname = path.resolve(__dirname, '../package.json');
 const pkg = JSON.parse(fs.readFileSync(pathname, 'utf-8'));
 let version = pkg.version;
 
-console.log('--------version', version)
+console.log('version:', version)
 
 const exec = require('child_process').exec;
-let cmdStr = `git commit -m "v${version}" && git push`;
+let cmdStr = `git commit -m "v${version}" && git push && git tag -a "v${version}" -m "${version}"`;
 exec(cmdStr, function (err, stdout, stderr) {
-    if (err) {
-        console.log('error:' + stderr);
-    } else {
-        var data = JSON.parse(stdout);
-        console.log('other:', data);
-    }
+    console.log('exec:', err, stdout, stderr);
 });
